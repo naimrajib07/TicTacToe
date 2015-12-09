f0721c29a7bc7123a0e653b51512720f5bb51128
Naim Rajiv <naim_cse07@yahoo.com>
2015-12-09 11:31:47 +0100

    Add game and board model validation.

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,5 +3,7 @@
 class Board < ActiveRecord::Base
   belongs_to :game
 
+  validates_presence_of :game_id, :current_player
+
   scope :latest_board, -> { order('created_at DESC').last }
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@
 # want to play TicTacToe.
 class Game < ActiveRecord::Base
   has_many :boards, dependent: :destroy
+
+  validates_presence_of :player_1_name, :player_2_name
 end
