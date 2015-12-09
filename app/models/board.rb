0968ef7e1212b374c8491b52fd8cd6b72972a0de
Naim Rajiv <naim_cse07@yahoo.com>
2015-12-09 09:31:20 +0100

    Game and Board Model  - Add toplevel comment for game and board model.  - Add scope in board model.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,7 @@
+# The <tt>Board</tt> class is used when you
+# want to play TicTacToe for a game with multiple round.
 class Board < ActiveRecord::Base
+  belongs_to :game
 
+  scope :latest_board, -> { order('created_at DESC').last }
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
+# The <tt>Game</tt> class is used when you
+# want to play TicTacToe.
 class Game < ActiveRecord::Base
+  has_many :boards, dependent: :destroy
 end
