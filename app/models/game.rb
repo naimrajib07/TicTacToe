7b27f21c59841fdedb58f6fc8c225ff21562893c
Naim Rajiv <naim_cse07@yahoo.com>
2015-12-11 02:25:14 +0100

    Add game score count model public method.

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,16 @@ class Game < ActiveRecord::Base
   has_many :boards, dependent: :destroy
 
   validates_presence_of :player_1_name, :player_2_name
+
+  def player_1_score
+    boards.player_1_won_count
+  end
+
+  def player_2_score
+    boards.player_2_won_count
+  end
+
+  def draw_score
+    boards.drawn_count
+  end
 end
