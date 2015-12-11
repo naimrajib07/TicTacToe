0868149c085e31b274143de37a8f578bfcbab8ca
Naim Rajiv <naim_cse07@yahoo.com>
2015-12-11 04:59:23 +0100

    - Add placeholder_with_error helper.  - Add css style  for form of new game.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Make placeholder text with error message if there is any validation error
+  def placeholder_with_error(object, attribute, title)
+    msg = title.humanize
+    if object.errors.any? && object.errors.messages[attribute]
+      msg += " #{object.errors.messages[attribute].join(', ')}"
+    end
+    msg
+  end
 end
