757658e0a1daaaac2a6ea1dce30392d952d93992
Naim Rajiv <naim_cse07@yahoo.com>
2015-12-11 03:46:15 +0100

    Add Gae model test code for public instance method.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,11 +8,35 @@ RSpec.describe Game, type: :model do
   let(:game) { FactoryGirl::build(:game) }
 
   describe 'Active Model Validation' do
-    it {  expect(game).to validate_presence_of(:player_1_name) }
-    it {  expect(game).to validate_presence_of(:player_2_name) }
+    it { expect(game).to validate_presence_of(:player_1_name) }
+    it { expect(game).to validate_presence_of(:player_2_name) }
   end
 
   describe 'ActiveRecord Associations' do
     it { expect(game).to have_many(:boards) }
+  end
+
+  describe 'public instance methods' do
+    context 'responds to its methods' do
+      it { expect(game).to respond_to(:player_1_score) }
+      it { expect(game).to respond_to(:player_2_score) }
+      it { expect(game).to respond_to(:draw_score) }
+    end
+
+    context 'executes methods correctly' do
+      context 'Game Score' do
+        it 'should get the player one score' do
+          expect(game.player_1_score).to eq(0)
+        end
+
+        it 'should get the second player score' do
+          expect(game.player_2_score).to eq(0)
+        end
+
+        it 'should get the game draw score' do
+          expect(game.draw_score).to eq(0)
+        end
+      end
+    end
   end
 end
